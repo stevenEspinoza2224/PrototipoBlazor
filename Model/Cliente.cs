4adf133664b6c2c71090f0be45214a8a0a75cabb
Steven Espinoza <74084123+stevenEspinoza2224@users.noreply.github.com>
2023-09-25 14:24:55 -0600

    Validaciones al Front

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -1,11 +1,19 @@
-﻿namespace Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model
 {
     public class Cliente
     {
 
         public int IdCliente { get; set; }
+
+        [Required(ErrorMessage ="La Categoría del Cliente es un campo requerido")]
         public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "El Nombre del Cliente es un campo requerido")]
         public string NombreCliente { get; set; }
+
+        [Required(ErrorMessage = "El Apellido del Cliente es un campo requerido")]
         public string ApellidoCliente { get; set; }
 
 
