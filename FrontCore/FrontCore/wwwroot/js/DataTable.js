bd4451c52e57980a8ba2b845b2453d7a36fd3759
Steven Espinoza <74084123+stevenEspinoza2224@users.noreply.github.com>
2023-09-25 10:43:15 -0600

    Se agrega DataTable.js

---
+++ FrontCore/FrontCore/wwwroot/js/DataTable.js
@@ -0,0 +1,28 @@
+﻿// Define una función para inicializar DataTables con un ID de tabla específico
+function inicializarDataTables(tableId) {
+    $(document).ready(function () {
+        // Utiliza el ID de la tabla proporcionado
+        $(`#${tableId}`).DataTable({
+            language: {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            }
+        });
+    });
+}
