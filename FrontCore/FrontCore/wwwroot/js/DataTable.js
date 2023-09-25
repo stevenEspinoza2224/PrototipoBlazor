ee4efb1457859dfd6afc438af529062b0a725d81
Steven Espinoza <74084123+stevenEspinoza2224@users.noreply.github.com>
2023-09-25 16:28:33 -0600

    Se agrega Sweet Alert

--- FrontCore/FrontCore/wwwroot/js/DataTable.js
+++ FrontCore/FrontCore/wwwroot/js/DataTable.js
@@ -26,3 +26,23 @@ export function inicializarDataTables(tableId) {
         });
     });
 }
+
+export function CustomConfirm(titulo, mensaje, tipo) {
+    return new Promise((resolve) => {
+        Swal.fire({
+            title: titulo,
+            text: mensaje,
+            icon: tipo,
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirmar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        })
+    });
+}
